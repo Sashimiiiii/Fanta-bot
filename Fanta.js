6e153a427aae7569a91649cbef491d5c0af9baf1
doriangat <doriangattesco@yahoo.fr>
2020-12-22 16:00:45 +0100

    Add files via upload

---
+++ Fanta.js
@@ -0,0 +1,96 @@
+const Discord = require("discord.js"); // So6
+const fs = require('fs');
+
+
+
+var bot = new Discord.Client();
+
+bot.on('ready', () => {        
+    console.log("BOT IS READY");
+    bot.user.setActivity("Salut", {type: "LISTENING",});
+})
+    
+
+
+
+
+    
+    
+
+  
+
+bot.commands = new Discord.Collection();
+
+function loadCmds() {
+    fs.readdir("./cmdss/ecrit/", (err, files) => {
+        if(err) console.erroe(err);
+        var jsFiles = files.filter(f => f.split(".").pop() === "js");
+        if(jsFiles.length <= 0) {
+            console.log("Aucune commande à chargé.")
+            return;
+        }
+        console.log(`${jsFiles.length} commandes chargées.`);
+        jsFiles.forEach((f, i) => {
+            delete require.cache[require.resolve(`./cmdss/ecrit/${f}`)];
+            var props = require(`./cmdss/ecrit/${f}`);
+            console.log(`${i + 1}: ${f} chargé`);
+            bot.commands.set(props.help.name, props); 
+        })
+    })
+};
+
+
+
+function loadCmds2() {
+    fs.readdir("./cmdss/vocal/", (err, files) => {
+        if(err) console.erroe(err);
+        var jsFiles = files.filter(f => f.split(".").pop() === "js");
+        if(jsFiles.length <= 0) {
+            console.log("Aucune commande à chargé.")
+            return;
+        }
+        console.log(`${jsFiles.length} commandes chargées.`);
+        jsFiles.forEach((f, i) => {
+            delete require.cache[require.resolve(`./cmdss/vocal/${f}`)];
+            var props = require(`./cmdss/vocal/${f}`);
+            console.log(`${i + 1}: ${f} chargé`);
+            bot.commands.set(props.help.name, props); 
+        })
+    })
+};
+loadCmds();
+loadCmds2();
+
+bot.on('message', message => {
+
+    if (message.author.bot) return
+    
+    
+    
+    
+    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+        prefixes: "f!"
+    };
+}
+
+let prefix = prefixes[message.guild.id].prefixes;
+    
+    
+
+     
+    
+var messageArray = message.content.split(/\s+/g);
+var args = messageArray.slice(1);
+var command = messageArray[0];
+var cmd = bot.commands.get(command.slice(prefix.length));
+if(!command.startsWith(prefix)) return;
+if(cmd) cmd.run(bot, message, args);    
+
+
+}) 
+
+bot.login("NzkwOTQ0ODI4MDU3MDU5MzM4.X-H-xg.zpZEXsVltbwi1jVkJkrqdUI12GM")
+
